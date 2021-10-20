b2eaa26e6498a263688dde467b36a6748f3cae86
vipul devnani <vipuldevnani97@gmail.com>
2021-10-20 12:59:54 +0530

    Created folder structure and initialised configurations and a basic components

---
+++ src/core/Base.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Base = ({
+    title="My title",
+    description="My description",
+    className="bg-dark text-white p-4",
+    children
+}) => {
+    return(
+        <div>
+            <div className='container-fluid'>
+                <div className='jumbotron bg-dark text-white text-center'>
+                    <h2 className='display-4'>{title}</h2>
+                    <p className='lead'>{description}</p>
+                </div>
+                <div className={className}>
+                    {children}
+                </div>
+            </div>
+            <div className='footer bg-dark mt-auto py-3'>
+                <div className='container bg-success text-white text-center py-3'>
+                    <h4>If you got any questions , feel free to reach out</h4>
+                    <button className='btn btn-warning btn-lg'>Contact Us</button>
+                </div>
+                <div className='container-fluid'>
+                    <span className='text-muted'>An Amazing <span className='text-white'> MERN </span> Bootcamp</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Base;
